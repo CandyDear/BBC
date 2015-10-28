ab3240fc7d29a1da13cc4861f6fc3bd49be5d751
mac <mac@macdeMacBook-Air-6.local>
2015-10-28 13:44:15 +0800

    Initial Commit

---
+++ BBC_News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBC_News
+//
+//  Created by mac on 15/10/28.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
